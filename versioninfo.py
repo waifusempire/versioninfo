bdedfec616cbb6654f769efb6ae253be893498bf
_waifus_empire_ <82737350+waifusempire@users.noreply.github.com>
2022-07-11 04:25:24 +0200

    Add files via upload

--- versioninfo.py
+++ versioninfo.py
@@ -6,21 +6,22 @@ from typing import Any, Union
 class ReleaseLevel(Enum):
     alpha = "alpha"
     beta = "beta"
+    final = "final"
     release = "release"
 
 
 @dataclass
 class VersionInfo:
+    name: str
     major: int
     minor: int = field(default=0)
     micro: int = field(default=0)
     releaselevel: ReleaseLevel = field(
-        default=ReleaseLevel.release, init=False)
-    serial: Union[int, None] = field(default=None, init=False)
-    name: Union[str, None] = field(default=None, init=False)
+        default=ReleaseLevel.final, init=False)
+    serial: int = field(default=0, init=False)
 
     def __repr__(self) -> str:
-        if self.releaselevel == ReleaseLevel.release:
+        if self.releaselevel == ReleaseLevel.final:
             return f"""<{self.__class__.__name__}{"" if not self.name else f" of '{self.name}'"} major={self.major}, minor={self.minor}, micro={self.micro}>"""
         elif self.releaselevel == ReleaseLevel.beta:
             if self.serial:
@@ -32,6 +33,11 @@ class VersionInfo:
                 return f"""<{self.__class__.__name__}{"" if not self.name else f" of '{self.name}'"} major={self.major}, minor={self.minor}, micro={self.micro}, releaselevel=alpha, serial={self.serial}>"""
             else:
                 return f"""<{self.__class__.__name__}{"" if not self.name else f" of '{self.name}'"} major={self.major}, minor={self.minor}, micro={self.micro}, releaselevel=alpha>"""
+        elif self.releaselevel == ReleaseLevel.release:
+            if self.serial:
+                return f"""<{self.__class__.__name__}{"" if not self.name else f" of '{self.name}'"} major={self.major}, minor={self.minor}, micro={self.micro}, releaselevel=release, serial={self.serial}>"""
+            else:
+                return f"""<{self.__class__.__name__}{"" if not self.name else f" of '{self.name}'"} major={self.major}, minor={self.minor}, micro={self.micro}, releaselevel=release>"""
 
     def __getitem__(self, __name: str):
         "x.__getitem__(y) <==> x[y]"
@@ -43,18 +49,18 @@ class VersionInfo:
     def to_dict(self) -> dict[str, Any]:
         return dict(name=self.name, major=self.major, minor=self.minor, micro=self.micro, releaselevel=self.releaselevel.value, serial=self.serial)
 
-    def set_releaselevel(self, releaselevel: ReleaseLevel, serial: Union[int, None] = None):
+    def set_releaselevel(self, releaselevel: ReleaseLevel, serial: int = 0):
         "Set a release level for the package/library/class"
         if not isinstance(releaselevel, ReleaseLevel):
             raise TypeError(
                 f"'releaselevel' must be an instance of 'ReleaseLevel' not of '{type(releaselevel).__name__}'")
         if not isinstance(serial, int) and not isinstance(serial, type(None)):
-            raise TypeError("'serial' may only be an int or None")
+            raise TypeError("'serial' may only be an int")
         self.releaselevel = releaselevel
         self.serial = serial
         return self
 
-    def set_name(self, __name: str):
+    def change_name(self, __name: str):
         "Set the name of the package/library/class"
         if not isinstance(__name, str):
             raise TypeError("'name' may only be of type 'str'")
@@ -62,4 +68,30 @@ class VersionInfo:
             raise ValueError("'name' can't be an empty string")
         self.name = __name
         return self
-    
+
+    def to_ver_string(self):
+        "name==a.b.cXY"
+        if self.releaselevel == ReleaseLevel.final:
+            s = f"{self.name}=={self.major}.{self.minor}.{self.micro}"
+        elif self.releaselevel == ReleaseLevel.beta:
+            s = f"{self.name}=={self.major}.{self.minor}.{self.micro}b{'' if not self.serial else self.serial}"
+        elif self.releaselevel == ReleaseLevel.alpha:
+            s = f"{self.name}=={self.major}.{self.minor}.{self.micro}a{'' if not self.serial else self.serial}"
+        elif self.releaselevel == ReleaseLevel.release:
+            s = f"{self.name}=={self.major}.{self.minor}.{self.micro}rc{'' if not self.serial else self.serial}"
+        return s
+
+    def to_vstring(self):
+        "va.b.cXY"
+        s = f"v{self.major}.{self.minor}.{self.micro}"
+        if self.releaselevel == ReleaseLevel.final:
+            pass
+        elif self.releaselevel == ReleaseLevel.release:
+            s+="rc{s}".format(s=self.serial)
+        elif self.releaselevel == ReleaseLevel.beta:
+            s+="b{s}".format(s=self.serial)
+        elif self.releaselevel == ReleaseLevel.alpha:
+            s+="a{s}".format(s=self.serial)
+        else:
+            pass
+        return s
